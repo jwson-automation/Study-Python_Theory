985d500214e48317264daa058a1fa711c46ef4d8
jwson-automation <sonjungwoo9@gmail.com>
2022-07-21 23:06:27 +0900

    1st week update

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,11 @@
 #!C:\Users\jwson\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7
 # print("hello,world")
-print(1+1)
+x = input('Could I ask your age?')
+birth = int(x)
+
+birthday=input('Have you already had your birthday?(y/n)')
+
+if birthday=='y':
+    print('Us age: ',birth-1)
+else:
+    print('Us age: ',birth-2)
